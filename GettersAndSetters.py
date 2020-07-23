116b50a75b84e039839fb7584a575161889e935d
Edgar Mancilla <delkoman@gmail.com>
2020-07-22 17:14:57 -0700

    Getters and Setters

---
+++ GettersAndSetters.py
@@ -0,0 +1,20 @@
+class CasillaVotacion:
+    def __init__(self, indentificador,pais):
+        self._identificador = indentificador
+        self._pais = pais
+        self._region = None
+    @property
+    def region(self):
+        return self._region
+    @region.setter
+    def region(self,reg):
+        if reg in self._pais:
+            self._region = reg
+        else:
+            raise ValueError('La region {region} no es valida')
+
+casilla = CasillaVotacion(123,['Bogota','Medellin'])
+print(casilla.region)
+casilla.region = 'China'
+print(casilla.region)
+raise ValueError('La region' + region + 'no es valida')
