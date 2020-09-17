599d84c9aa5edb79d90eee47d138124ef5005810
Edgar Mancilla <delkoman@gmail.com>
2020-09-17 10:58:51 -0700

    primer commit reto en progreso

---
+++ RetoHerencia.py
@@ -0,0 +1,9 @@
+class Planta:
+
+    def __init__(self, tipo, uso, iluminacion):
+        self.tipo = tipo
+        self.uso = uso
+        self.iluminacion = iluminacion
+
+    def infoplanta(self):
+        print('La ')
